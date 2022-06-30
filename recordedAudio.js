a8b063a3314ac2131698fc64dc4654661a02b64c
shankardesigner <jaspershankar@live.com>
2022-06-30 18:26:53 +0545

    added different types of audio

--- recordedAudio.js
+++ recordedAudio.js
@@ -11,8 +11,18 @@ const deepgramApiKey = 'YOUR_DEEPGRAM_API_KEY'
 // Example of a remote file: https://static.deepgram.com/examples/interview_speech-analytics.wav
 // const file = 'https://static.deepgram.com/examples/interview_speech-analytics.wav'
   //  const file = `https://developers.deepgram.com/data/audio/premier_broken-phone.mp3`;
-  const file = `/Users/sbhattarai/Downloads/Transcription - Noise + 2speakers.m4a`
 
+  //runs successfully but couldn't detect the speakers
+  // const file = `/home/fm-pc-lt-105/Downloads/Transcription- Noise + 2Speakers in wav format.wav`
+  // const file = `/home/fm-pc-lt-105/Downloads/Transcription - Noise + 2speakers.m4a`
+  // const file = `/home/fm-pc-lt-105/Downloads/life-moves-pretty-fast.wav`
+  // const file = `/home/fm-pc-lt-105/Downloads/interview_speech-analytics.wav`
+
+  //clear and extremely noisy audio
+  // const file = `/home/fm-pc-lt-105/Downloads/Clear Audio Phone Call.m4a`;
+  const file = `/home/fm-pc-lt-105/Downloads/Extremly Noisy Audio.m4a`
+  
+  
 // Mimetype for the file you want to transcribe
 // Only necessary if transcribing a local file
 // Example: audio/wav
